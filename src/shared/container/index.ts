a301320d65ebfc8107b612a9ff12d1c052b6aa13
Lucas Menezes <luckmenez@gmail.com>
2023-01-08 18:59:01 -0300

    fix: removed the repository delay

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,4 @@
-import { container, delay } from "tsyringe";
+import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
@@ -21,7 +21,7 @@ container.registerSingleton<ISpecificationsRepository>(
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
-  delay(() => UsersRepository)
+  UsersRepository
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
