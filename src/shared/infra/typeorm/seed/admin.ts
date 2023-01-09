23b4a902b3676d6e57e4a2119acc228fff2cc4ba
Lucas Menezes <luckmenez@gmail.com>
2023-01-08 22:48:40 -0300

    fix: admin password with async functions

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -7,10 +7,10 @@ async function create() {
   const connection = await createConnection("localhost");
 
   const id = uuidV4();
-  const password = hash("admin", 8);
+  const password = await hash("admin", 8);
 
   await connection.query(
-    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license ) 
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license )
       values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
     `
   );
