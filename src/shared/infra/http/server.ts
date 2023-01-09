f5fde0541871b879661bc8201fabf6d9cf69f78c
Lucas Menezes <luckmenez@gmail.com>
2023-01-08 22:46:28 -0300

    fix: fixed container deleted import

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,6 +3,7 @@ import "reflect-metadata";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import { router } from "@shared/infra/http/routes";
 import createConnection from "@shared/infra/typeorm";
