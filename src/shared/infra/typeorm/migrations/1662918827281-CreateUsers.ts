9a2766c672b9ffa29605e71dbf7b7f84dd99a805
Lucas Menezes <luckmenez@gmail.com>
2023-01-23 17:14:29 -0300

    fix: adding isPrimary tag to CreateUser migration

--- src/shared/infra/typeorm/migrations/1662918827281-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1662918827281-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1662918827281 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
