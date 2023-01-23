c98afd8e04c08c4243ee944bfa43ad0839ee331b
Lucas Menezes <luckmenez@gmail.com>
2023-01-23 17:12:27 -0300

    feat: maping of CarImagesRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,11 +2,13 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/implementations/CarsImageRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/implementations/CarsRepositories";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/implementations/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/implementations/SpecificationsRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoryRepository } from "@modules/cars/repositories/ICategoryRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/in-memory/ICarsImagesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoryRepository>(
@@ -25,3 +27,8 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>(
+  "CarsImagesRepository",
+  CarsImagesRepository
+);
