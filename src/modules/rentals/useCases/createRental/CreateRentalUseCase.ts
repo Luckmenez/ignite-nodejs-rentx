0781a18399ec412f0d7a9228f2bd21d2ea4a62f8
Lucas Menezes <luckmenez@gmail.com>
2023-01-26 21:46:11 -0300

    feat: injection of dateProvider inside CreateRentalUseCase

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,4 +1,6 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -8,13 +10,17 @@ interface IRequest {
 }
 
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    private dateProvider: IDateProvider,
+    private rentalsRepository: IRentalsRepository
+  ) {}
 
   async execute({
     user_id,
     car_id,
     expected_return_date,
-  }: IRequest): Promise<void> {
+  }: IRequest): Promise<Rental> {
+    const minimumRentTimeInHours = 24;
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id
     );
@@ -30,6 +36,25 @@ class CreateRentalUseCase {
     if (rentalOpenToUser) {
       throw new AppError("there's a rental in progress for this user");
     }
+
+    const dateNow = this.dateProvider.dateNow();
+
+    const compareDate = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
+
+    if (compareDate < minimumRentTimeInHours) {
+      throw new AppError("Couldn't rent a rent with less than 24 hours");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
   }
 }
 
