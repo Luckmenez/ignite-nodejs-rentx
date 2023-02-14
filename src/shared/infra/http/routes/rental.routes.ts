01e6731630dc7c89daa7b3ab88fd0572035ae6b7
Lucas Menezes <luckmenez@gmail.com>
2023-02-14 20:56:25 -0300

    fix: fixed wrong controller call

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { RentalDevolutionController } from "@modules/rentals/useCases/rentalDevolution/RentalDevolutionController";
 
 import { ensureAuthentication } from "../middlewares/ensureAuthentication";
@@ -9,12 +10,18 @@ const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const rentalDevolutionController = new RentalDevolutionController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthentication, createRentalController.handle);
 rentalRoutes.post(
   "/devolution/:id",
   ensureAuthentication,
   rentalDevolutionController.handle
+);
+rentalRoutes.get(
+  "/user",
+  ensureAuthentication,
+  listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
