a20ff46f8d6b4188b273940d8d6a7691b9b0fbf5
Lucas Menezes <luckmenez@gmail.com>
2023-02-13 23:05:08 -0300

    feat: implementation of RentalDevolution Controller

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { RentalDevolutionController } from "@modules/rentals/useCases/rentalDevolution/RentalDevolutionController";
 
 import { ensureAuthentication } from "../middlewares/ensureAuthentication";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const rentalDevolutionController = new RentalDevolutionController();
 
 rentalRoutes.post("/", ensureAuthentication, createRentalController.handle);
+rentalRoutes.post(
+  "/devolution/:id",
+  ensureAuthentication,
+  rentalDevolutionController.handle
+);
 
 export { rentalRoutes };
