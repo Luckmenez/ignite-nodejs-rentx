8b0153712107a8b2bff8543f54737d91d285cf4f
Lucas Menezes <luckmenez@gmail.com>
2023-02-23 21:22:28 -0300

    feat: created auth configs

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "f2091e84e0edfd087217848712c98b99",
+  expires_in: "15m",
+  secret_refresh_token: "8e0ff26aeeb3ec4538acd5c9730e38e8",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
