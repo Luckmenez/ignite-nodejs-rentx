2dfb12711c4620b89222693575c7b6ab64825844
Lucas Menezes <luckmenez@gmail.com>
2023-05-25 00:14:19 -0300

    feat: implementation of refresh token authentication

--- src/shared/infra/http/middlewares/ensureAuthentication.ts
+++ src/shared/infra/http/middlewares/ensureAuthentication.ts
@@ -1,7 +1,8 @@
 import { Response, Request, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -14,22 +15,23 @@ export async function ensureAuthentication(
   next: NextFunction
 ) {
   const authHeader = request.headers.authorization;
+  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
-    throw new AppError("Missing Token", 401);
+    throw new AppError("Missing Token1", 401);
   }
 
   const [, token] = authHeader.split(" ");
   try {
     const { sub: user_id } = verify(
       token,
-      "f2091e84e0edfd087217848712c98b99"
+      auth.secret_refresh_token
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
+    const refreshToken =
+      await usersTokensRepository.findByUserIdAndRefreshToken(user_id, token);
 
-    if (!user) {
+    if (!refreshToken) {
       throw new AppError("User does not exists", 401);
     }
 
@@ -39,6 +41,6 @@ export async function ensureAuthentication(
 
     next();
   } catch (error) {
-    throw new AppError("Missing Token", 401);
+    throw new AppError("Invalid token", 401);
   }
 }
