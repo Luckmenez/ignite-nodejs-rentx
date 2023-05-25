2aef7e860b03f6815dc0cee4023be671f54d5726
Lucas Menezes <luckmenez@gmail.com>
2023-05-25 00:13:44 -0300

    feat: implementation of authentication routes

--- src/shared/infra/http/routes/authentication.routes.ts
+++ src/shared/infra/http/routes/authentication.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 import { UserAuthenticationController } from "@modules/accounts/useCases/userAuthentication/userAuthenticationController";
 
 const authenticationRoutes = Router();
 
 const userAuthenticationController = new UserAuthenticationController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticationRoutes.post("/sessions", userAuthenticationController.handle);
+authenticationRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticationRoutes };
