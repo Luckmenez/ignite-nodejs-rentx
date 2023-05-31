b6593f79b35233ece569bedb5a340128e930d9a8
Lucas Menezes <luckmenez@gmail.com>
2023-05-30 22:57:15 -0300

    feat: adding routes for reset password

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetUserPasswordController } from "@modules/accounts/useCases/resetUserPassword/ResetUserPasswordController";
 import { SendForgotMailPasswordController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotMailPasswordController";
 
 const passwordRoutes = Router();
 
 const sendForgotMailPasswordController = new SendForgotMailPasswordController();
+const resetUserPasswordController = new ResetUserPasswordController();
 
 passwordRoutes.post("/forgot", sendForgotMailPasswordController.handle);
+passwordRoutes.post("/reset", resetUserPasswordController.handle);
 
 export { passwordRoutes };
